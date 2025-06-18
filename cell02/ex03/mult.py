ff81602d495d43a876df70d2c36673320d61d48e
jojoceo <Pathaninjj04@gmail.com>
2025-06-18 20:34:59 +0700

    Create mult.py

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+f_num = int(input("Enter the first number:\n"))
+s_num = int(input("Enter the second number:\n"))
+
+result = f_num * s_num
+
+print(f"{f_num} x {s_num} = {result}")
+
+if result > 0: print("The result is positive.")
+elif result < 0: print("The result is negative.")
+else: print("The result is positive and negative.")
