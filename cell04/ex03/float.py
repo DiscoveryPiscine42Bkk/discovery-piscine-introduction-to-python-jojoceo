71cb26f4591aa8759f1bc2008c776526c4ad3847
jojoceo <Pathaninjj04@gmail.com>
2025-06-19 00:08:23 +0700

    Create float.py

---
+++ cell04/ex03/float.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+num = float(input("Give me a number : "))
+
+decimal = num - int(num)
+
+if not decimal: print("This number is an integer.")
+else: print("This number is decimal.")
