7bcd313c2faab068a39e658df9dd3f92733f13ec
jojoceo <Pathaninjj04@gmail.com>
2025-06-19 18:26:45 +0700

    Create parameter_matching.py

---
+++ cell05/ex10/parameter_matching.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) < 2: print("none")
+else :     
+    text = str(input("What was the parameter? "))
+    if text == sys.argv[1]: print("Good job!")
+    else: print("Nope, sorry...")
