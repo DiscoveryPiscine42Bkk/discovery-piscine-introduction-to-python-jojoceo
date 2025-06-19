c2a957b94198de85bf01bd3f69d5d92fc18bd720
jojoceo <Pathaninjj04@gmail.com>
2025-06-19 12:32:29 +0700

    Create play_with_arrays.py

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+num_list = [2, 8, 9, 48, 8, 22, -12, 2]
+result = set()
+
+print(num_list)
+
+for i in num_list:
+    if i >= 5 and i + 2 not in result:
+        result.add(i + 2)
+
+print(result)
