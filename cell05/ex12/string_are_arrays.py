2a1ed0343fdfc15c538d59e3119fda122f1f1d29
jojoceo <Pathaninjj04@gmail.com>
2025-06-19 18:29:33 +0700

    Create string_are_arrays.py

---
+++ cell05/ex12/string_are_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+z = 0
+
+if len(sys.argv) < 2: print("none")
+else: 
+    for i in sys.argv[1]:
+        if i ==  "z" or i == "Z":
+            z += 1
+
+print("z"*z)
